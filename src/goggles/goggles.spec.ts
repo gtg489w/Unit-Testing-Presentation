8640d2ddc8d9ef2df77cd7ff51643f58e804c8b5
Brian O'Connor <gtg489w@gmail.com>
2019-08-26 08:31:27 -0400

    Added unit tests to verify name attribute on class

--- src/goggles/goggles.spec.ts
+++ src/goggles/goggles.spec.ts
@@ -14,4 +14,16 @@ describe('Goggles', () => {
 		const result = goggles.getName();
 		expect(result).to.equal('Goggles');
 	});
+
+	it('should save my name when creating with constructor', () => {
+		var goggles = new Goggles('My Goggles');
+		const result = goggles.getName();
+		expect(result).to.equal('My Goggles');
+	});
+
+	it('should save my name when set', () => {
+		goggles.setName('Ski Goggles');
+		const result = goggles.getName();
+		expect(result).to.equal('Ski Goggles');
+	});
 });
